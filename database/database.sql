02929ac714c9bd6f476b375789a4fa5a06fa6d2d
sbenstewart <sbenstewart@gmail.com>
2018-12-27 06:28:06 +0530

    Database added

---
+++ database/database.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Dec 27, 2018 at 12:52 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+CREATE DATABASE IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `database`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accomodation`
+--
+
+DROP TABLE IF EXISTS `accomodation`;
+CREATE TABLE IF NOT EXISTS `accomodation` (
+  `id` int(11) NOT NULL,
+  `cnt` int(11) NOT NULL DEFAULT '1',
+  `hours` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `accomodation`
+--
+
+TRUNCATE TABLE `accomodation`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ambassador`
+--
+
+DROP TABLE IF EXISTS `ambassador`;
+CREATE TABLE IF NOT EXISTS `ambassador` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phoneno` varchar(10) NOT NULL,
+  `emailid` varchar(100) NOT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `ambassador`
+--
+
+TRUNCATE TABLE `ambassador`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `amb_registrations`
+--
+
+DROP TABLE IF EXISTS `amb_registrations`;
+CREATE TABLE IF NOT EXISTS `amb_registrations` (
+  `aid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `count` int(11) NOT NULL,
+  KEY `id` (`id`),
+  KEY `aid` (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `amb_registrations`
+--
+
+TRUNCATE TABLE `amb_registrations`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `event`
+--
+
+TRUNCATE TABLE `event`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrations`
+--
+
+DROP TABLE IF EXISTS `registrations`;
+CREATE TABLE IF NOT EXISTS `registrations` (
+  `cid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cid` (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `registrations`
+--
+
+TRUNCATE TABLE `registrations`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phoneno` varchar(10) NOT NULL,
+  `emailid` varchar(100) NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `user`
+--
+
+TRUNCATE TABLE `user`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workshop`
+--
+
+DROP TABLE IF EXISTS `workshop`;
+CREATE TABLE IF NOT EXISTS `workshop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `workshop`
+--
+
+TRUNCATE TABLE `workshop`;
+--
+-- Constraints for dumped tables
+--
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

b642df99af8b681808da7d8b9e941cc234551c35
sbenstewart <sbenstewart@gmail.com>
2018-12-29 16:20:00 +0530

    php db connectivity working

--- docs/phpmysqlconnect.php
+++ docs/phpmysqlconnect.php
@@ -1,20 +1,17 @@
 <?php
 require_once 'dbconfig.php';
-
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Connected to $dbname at $host successfully.";
-} catch (PDOException $pe) {
-    die("Could not connect to the database $dbname :" . $pe->getMessage());
-}
-
 $name2 = $_POST['name1'];
 $mail2 = $_POST['mail1'];
 $phone2 = $_POST['phone1'];
 
-if (isset($_POST['name1'])) {
-$query = mysql_query("insert into user(name, emailid, phoneno) values ('$name2', '$email2', '$phone2')"); //Insert Query
-echo "Form Submitted succesfully";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    echo "Connected to $dbname at $host successfully.";
+    $count = $conn->exec("insert into user(name, emailid, phoneno) values ('$name2', '$mail2', '$phone2')");
+
+    echo "Form Submitted succesfully $count";
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
-mysql_close($conn); // Connection Closed
+ // Connection Closed
 ?>

ed817fbcf1e53f90694e83ca584fa392884fc141
sbenstewart <sbenstewart@gmail.com>
2018-12-29 09:34:50 +0530

    query fixes

--- docs/js/ajaxjs.php
+++ docs/js/ajaxjs.php
@@ -3,10 +3,10 @@
 $name2 = $_POST['name1'];
 $mail2 = $_POST['mail1'];
 $phone2 = $_POST['phone1'];
-$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
-$db = mysql_select_db("mydba", $connection); // Selecting Database
+$connection = mysql_connect("localhost", "root", "root"); // Establishing Connection with Server..
+$db = mysql_select_db("database", $connection); // Selecting Database
 if (isset($_POST['name1'])) {
-$query = mysql_query("insert into form_element(name, mail, phone) values ('$name2', '$email2', '$password2','$phone2')"); //Insert Query
+$query = mysql_query("insert into user(name, emailid, phoneno) values ('$name2', '$email2', '$phone2')"); //Insert Query
 echo "Form Submitted succesfully";
 }
 mysql_close($connection); // Connection Closed

37a7a8253329c74182579bfcfa679fc9f1c552b1
sbenstewart <sbenstewart@gmail.com>
2018-12-29 17:15:41 +0530

    php mysql connectivity done

--- docs/phpmysqlconnect.php
+++ docs/phpmysqlconnect.php
@@ -8,8 +8,15 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     echo "Connected to $dbname at $host successfully.";
     $count = $conn->exec("insert into user(name, emailid, phoneno) values ('$name2', '$mail2', '$phone2')");
+    foreach ($conn->query("SELECT cid from user where name='$name2'") as $row)
+    {
+      $id = $row['cid'];
+    }
+    $cookie_name = "user";
+    $cookie_value = $id;
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
 
-    echo "Form Submitted succesfully $count";
+    echo "Form Submitted succesfully $id";
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }

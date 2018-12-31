68f193b29f194e70b1cba4f7b901b6ff76c51be9
sbenstewart <sbenstewart@gmail.com>
2018-12-31 10:18:54 +0530

    registerevent uploaded

--- docs/js/main.js
+++ docs/js/main.js
@@ -26,14 +26,14 @@ function regEvent() {
   var mail = 'temp';
   var phone = 'temp';
   // Returns successful data submission message when the entered information is stored in database.
-  var dataString = 'name1=' + name + '&mail1=' + mail + '&phone1=' + phone;
+  var dataString = 'event1=' + "sample";
   if (name == '' || mail == '' || phone == '') {
   alert("Please Fill All Fields");
   } else {
   // AJAX code to submit form.
   jQuery.ajax({
   type: "POST",
-  url: "registeruser.php",
+  url: "registerevent.php",
   data: dataString,
   cache: false,
   success: function(html) {

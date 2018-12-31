d43eb44a9b0d0a0025d05309e9adda72d449fdcd
sbenstewart <sbenstewart@gmail.com>
2018-12-31 15:13:17 +0530

    Added accomodatioin buuton

--- docs/js/main.js
+++ docs/js/main.js
@@ -21,6 +21,31 @@ alert(html);
 return false;
 }
 
+function regAccomodation() {
+var name = document.getElementById("name").value;
+var mail = document.getElementById("mail").value;
+var phone = document.getElementById("phone").value;
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name1=' + name + '&mail1=' + mail + '&phone1=' + phone;
+if (name == '' || mail == '' || phone == '') {
+alert("Please Fill All Fields");
+} else {
+// AJAX code to submit form.
+jQuery.ajax({
+type: "POST",
+url: "registeruser.php",
+data: dataString,
+cache: false,
+success: function(html) {
+alert(html);
+}
+});
+}
+return false;
+}
+
+
+
 function regEvent() {
   var name = 'temp';
   var mail = 'temp';

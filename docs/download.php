6b31511b8ba58a2d72c63ebd20fc78096d29069a
sbenstewart <sbenstewart@gmail.com>
2019-01-01 10:04:52 +0530

    sample admin php backend

---
+++ docs/download.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'dbconfig.php';
+$name2 = $_POST['name1'];
+$mail2 = $_POST['mail1'];
+$phone2 = $_POST['phone1'];
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //echo "Connected to $dbname at $host successfully.";
+    $count = $conn->exec("insert into user(name, emailid, phoneno) values ('$name2', '$mail2', '$phone2')");
+    foreach ($conn->query("SELECT cid from user where name='$name2'") as $row)
+    {
+      $id = $row['cid'];
+    }
+    $cookie_name = "user";
+    $cookie_value = $id;
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
+
+    if(!isset($_COOKIE[$cookie_name]))
+    {
+      echo "Cookie named '" . $cookie_name . "' is not set!";
+    }
+    else
+    {
+      echo "Cookie '" . $cookie_name . "' is set!";
+      echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+
+    echo "Form Submitted succesfully $id";
+
+
+    if (isset($_POST['event1_download'])) {
+    //download event
+} else if (isset($_POST['accomodation_download'])) {
+    //download accomodation
+} else {
+    //no button pressed
+}
+
+
+
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+ // Connection Closed
+?>

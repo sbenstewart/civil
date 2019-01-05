324c727b62a20b176765feadda6c4ab0167583d5
sbenstewart <sbenstewart@gmail.com>
2019-01-05 19:25:00 +0530

    User change details page

--- docs/js/main.js
+++ docs/js/main.js
@@ -96,3 +96,32 @@ alert(html);
 }
 return false;
 }
+
+function editUser() {
+var name = document.getElementById("name").value;
+var mail = document.getElementById("mail").value;
+var phone = document.getElementById("phone").value;
+var code = document.getElementById("code").value;
+var college = document.getElementById("college").value;
+var year = document.getElementById("year").value;
+var dept = document.getElementById("dept").value;
+var course = document.getElementById("course").value;
+var cid = document.getElementById("cid").value;
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name1=' + name + '&mail1=' + mail + '&phone1=' + phone + '&code1=' + code+ '&college1=' + college+ '&year1=' + year+ '&dept1=' + dept + '&course1=' + course + '&cid1=' + cid;
+if (name == '' || mail == '' || phone == '') {
+alert("Please Fill All The Required Fields");
+} else {
+// AJAX code to submit form.
+jQuery.ajax({
+type: "POST",
+url: "edituser.php",
+data: dataString,
+cache: false,
+success: function(html) {
+alert(html);
+}
+});
+}
+return false;
+}

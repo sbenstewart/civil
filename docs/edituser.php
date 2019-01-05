da671d95d55b3a392e0aa600d76124ed2c909774
sbenstewart <sbenstewart@gmail.com>
2019-01-05 20:01:20 +0530

    code optional added

--- docs/edituser.php
+++ docs/edituser.php
@@ -9,52 +9,72 @@ $year2 = $_POST['year1'];
 $dept2 = $_POST['dept1'];
 $course2 = $_POST['course1'];
 $cid2 = $_POST['cid1'];
+var $aid2;
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     //echo "Connected to $dbname at $host successfully.";
+
+    $sql = "SELECT COUNT(*) from ambassador where code='$code2'";
+    if ($res = $conn->query($sql)) {
+
+        /* Check the number of rows that match the SELECT statement */
+        if ($res->fetchColumn() > 0) {
+          foreach ($conn->query("SELECT aid from ambassador where code='$code2'") as $row)
+          {
+            $aid2 = $row['aid'];
+          }
+
+    }
+
+
+
+
+
 
     $sql = "SELECT COUNT(*) from user where cid='$cid2' and emailid='$mail2'";
     if ($res = $conn->query($sql)) {
 
         /* Check the number of rows that match the SELECT statement */
         if ($res->fetchColumn() > 0) {
-          foreach ($conn->query("SELECT aid from user where cid='$cid2'") as $row)
+
+          $count = $conn->exec("update user set name='$name2', emailid='$mail2', phoneno='$phone2', aid='$aid2', college='$college2', year='$year2', dept='$dept2', course='$course2' where cid='$cid2' ");
+          echo "succesfully updated your details."
+
+
+          foreach ($conn->query("SELECT cid from user where emailid='$mail2'") as $row)
           {
-            $id = $row['aid'];
+            $id = $row['cid'];
           }
-          $count = $conn->exec("insert into user(name, emailid, phoneno, aid, college, year, dept, course) values ('$name2', '$mail2', '$phone2', '$id', '$college2', '$year2', '$dept2', '$course2')");
+          $cookie_name = "user";
+          $cookie_value = $id;
+          setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
 
+          if(!isset($_COOKIE[$cookie_name]))
+          {
+            echo "Cookie named '" . $cookie_name . "' is not set!";
+          }
+          else
+          {
+            echo "Cookie '" . $cookie_name . "' is set!";
+            echo "Value is: " . $_COOKIE[$cookie_name];
+          }
+
+          
+
+          }
+          /* No rows matched -- do something else */
+          else {
+            echo "cid and mail did not match our records. Please register again.";
+          }
+
+
+
+
+          //echo "Form Submitted succesfully $id";
 
         }
-        /* No rows matched -- do something else */
-        else {
-          $count = $conn->exec("insert into user(name, emailid, phoneno, college, year, dept, course) values ('$name2', '$mail2', '$phone2', '$college2', '$year2', '$dept2', '$course2')");
 
-        }
-
-
-    foreach ($conn->query("SELECT cid from user where emailid='$mail2'") as $row)
-    {
-      $id = $row['cid'];
-    }
-    $cookie_name = "user";
-    $cookie_value = $id;
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
-
-    if(!isset($_COOKIE[$cookie_name]))
-    {
-      echo "Cookie named '" . $cookie_name . "' is not set!";
-    }
-    else
-    {
-      echo "Cookie '" . $cookie_name . "' is set!";
-      echo "Value is: " . $_COOKIE[$cookie_name];
-    }
-
-    echo "Form Submitted succesfully $id";
-
-  }
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }

3f9d7148c75de9a6cb12b3ec187eb65802cd95db
sbenstewart <sbenstewart@gmail.com>
2019-01-06 22:14:23 +0530

    Forget password working

--- docs/forgotpassword.php
+++ docs/forgotpassword.php
@@ -2,31 +2,45 @@
 require_once 'dbconfig.php';
 $mail2 = $_POST['mail1'];
 
+function random_num($size) {
+	$alpha_key = '';
+	$keys = range('A', 'Z');
+
+	for ($i = 0; $i < 2; $i++) {
+		$alpha_key .= $keys[array_rand($keys)];
+	}
+
+	$length = $size - 2;
+
+	$key = '';
+	$keys = range(0, 9);
+
+	for ($i = 0; $i < $length; $i++) {
+		$key .= $keys[array_rand($keys)];
+	}
+
+	return $alpha_key . $key;
+}
+
+$code=random_num(6);
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     echo "Connected to $dbname at $host successfully.";
-    
+    $sql = "SELECT COUNT(*) from user where emailid='$mail2'";
+    if ($res = $conn->query($sql)) {
 
-    foreach ($conn->query("SELECT cid from user where emailid='$mail2' and password='$password2'") as $row)
-    {
-      $id = $row['cid'];
-
-    $cookie_name = "user";
-    $cookie_value = $id;
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
-
-    if(!isset($_COOKIE[$cookie_name]))
-    {
-      //echo "Cookie named '" . $cookie_name . "' is not set!";
+        /* Check the number of rows that match the SELECT statement */
+        if ($res->fetchColumn() > 0) {
+          $count = $conn->exec("update user set password='$code' where emailid='$mail2'");
+          echo "New password is $code";
+        }
     }
-    else
-    {
-      echo "Cookie '" . $cookie_name . "' is set!";
-      echo "Value is: " . $_COOKIE[$cookie_name];
+    else {
+        echo "No such mail id found in the server. Please register.";
     }
 
-  }
-    echo "Form Submitted succesfully $id";
+
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }

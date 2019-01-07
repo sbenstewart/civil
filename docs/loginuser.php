2e45763da3e77053d8046322ebe32925025b3aa1
sbenstewart <sbenstewart@gmail.com>
2019-01-07 13:17:09 +0530

    login.php session

--- docs/loginuser.php
+++ docs/loginuser.php
@@ -5,7 +5,7 @@ $mail2 = $_POST['mail1'];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Connected to $dbname at $host successfully.";
+    //echo "Connected to $dbname at $host successfully.";
     //echo "insert into user(name, emailid, phoneno, college, year, dept, course) values ('$name2', '$mail2', '$phone2', '$college2', '$year2', '$dept2', '$course2')";
 
 
@@ -13,22 +13,13 @@ try {
     {
       $id = $row['cid'];
 
-    $cookie_name = "user";
-    $cookie_value = $id;
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30*31), "/");//one month
+      session_start();
+      /*session is started if you don't write this line can't use $_Session  global variable*/
+      $_SESSION["cid"]=$id;
 
-    if(!isset($_COOKIE[$cookie_name]))
-    {
-      //echo "Cookie named '" . $cookie_name . "' is not set!";
-    }
-    else
-    {
-      echo "Cookie '" . $cookie_name . "' is set!";
-      echo "Value is: " . $_COOKIE[$cookie_name];
-    }
-    
+      echo "Logged in with cid as $_SESSION["cid"]";
+
   }
-    echo "Form Submitted succesfully $id";
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }

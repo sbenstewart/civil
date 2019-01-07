60f2db64be3cc09812dab90392f8712c8222e231
sbenstewart <sbenstewart@gmail.com>
2019-01-07 13:50:38 +0530

    Update accomodation

--- docs/registeraccomodation.php
+++ docs/registeraccomodation.php
@@ -20,7 +20,7 @@ try {
       //echo $_SESSION["cid"];
       $cid2=$_SESSION["cid"];
 
-      $count = $conn->exec("insert into accomodation(id, entrydate, entrytime, hours) values ('$cid2', '$date2', '$time2' ,'$hours2')");
+      $count = $conn->exec("insert into accomodation(id, entrydate, entrytime, hours) values ('$cid2', '$date2', '$time2' ,'$hours2') ON DUPLICATE KEY UPDATE entrydate='$date2', entrytime='$time2', hours='$hours2' ");
       foreach ($conn->query("SELECT name from user where cid='$cid2'") as $row)
       {
         $name = $row['name'];

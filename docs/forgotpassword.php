ab1eb434bae930f17d2bcf031560a33e8bde2e58
sbenstewart <sbenstewart@gmail.com>
2019-01-07 15:31:17 +0530

    Mail client added

--- docs/forgotpassword.php
+++ docs/forgotpassword.php
@@ -33,7 +33,27 @@ try {
         /* Check the number of rows that match the SELECT statement */
         if ($res->fetchColumn() > 0) {
           $count = $conn->exec("update user set password='$code' where emailid='$mail2'");
-          echo "New password is $code";
+          //echo "New password is $code";
+
+					$to   = "sbenstewart@gmail.com";
+					$from = 'him@sbenstewart.in';
+
+					$subject = "Password Changed";
+
+					$headers = "From: " . $from . "\r\n";
+					$headers .= "Reply-To: ". $from . "\r\n";
+					$headers .= "CC: your_email\r\n";
+					$headers .= "MIME-Version: 1.0\r\n";
+					$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+					$message = '<html><body>';
+					$message .= '<h1>Civilisation 2019</h1>';
+					$message .= "Password is $code";
+					$message .= "</body></html>";
+
+					mail($to, $subject, $message, $headers);
+
+
         }
         else {
             echo "No such mail id found in the server. Please register.";

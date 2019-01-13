9c911a6ee6bebba5c52e2e2a7a1acd8ca8bcd311
sbenstewart <sbenstewart@gmail.com>
2019-01-13 22:02:39 +0530

    Offline LEGO_loader

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -4,7 +4,7 @@
 		loop: true,
 		prerender: true,
 		autoplay: true,
-		path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+		path: 'LEGO_loader.json'
 	};
 	var anim = bodymovin.loadAnimation(animData);
 anim.setSpeed(3.4);
@@ -14,6 +14,6 @@ $(document).ready(function($) {
     Body.addClass('preloader-site');
 });
 $(window).load(function() {
-    $('.preloader-wrapper').fadeOut();
+    $('.preloader-wrapper').delay(5000).fadeOut();
     $('body').removeClass('preloader-site');
 });

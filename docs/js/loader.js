47aa94a624dc199837ccd65cc1e5b3c8c1bc86a3
sbenstewart <sbenstewart@gmail.com>
2019-01-13 21:25:14 +0530

    Preloader finally working

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -1,5 +1,5 @@
 	var animData = {
-		wrapper: document.querySelector('#animationWindow'),
+		wrapper: document.querySelector('#preloader'),
 		animType: 'svg',
 		loop: true,
 		prerender: true,
@@ -8,3 +8,12 @@
 	};
 	var anim = bodymovin.loadAnimation(animData);
 anim.setSpeed(3.4);
+
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
+$(window).load(function() {
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});

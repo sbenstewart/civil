3fe96846a4be215525dcad957ade299222b1616a
sbenstewart <sbenstewart@gmail.com>
2019-01-13 09:36:59 +0530

    Preloader added

---
+++ docs/js/loader.js
@@ -0,0 +1,16 @@
+	var animData = {
+		wrapper: document.querySelector('#animationWindow'),
+		animType: 'svg',
+		loop: true,
+		prerender: true,
+		autoplay: true,
+		path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+	};
+	var anim = bodymovin.loadAnimation(animData);
+anim.setSpeed(3.4);
+
+$(document).ready(function() {
+window.onload = function () {
+$('#animationWindow').fadeOut(2500, function(){ $('#animationWindow').remove(); } );
+}
+});
---
+++ lego-loader/index.html
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>LEGO Loader</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  
+<div id="animationWindow">
+</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/bodymovin/4.8.0/bodymovin.min.js'></script>
+
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>

550f24bd974d3f6a36a452d32c51ad37fe83415b
sbenstewart <sbenstewart@gmail.com>
2019-01-26 19:02:59 +0530

    Revert "Admin page fixes"
    This reverts commit 85a89dea0b55f5843ba531784675817eae4bb8f1.

--- docs/download-event.php
+++ docs/download-event.php
@@ -21,11 +21,11 @@ try {
           }
       }
       //Setup the filename that our CSV will have when it is downloaded.
-      $fileName = 'mysql-events.csv';
+      $fileName = 'mysql-export.csv';
       ob_clean();
       header("Pragma: no-cache");
       header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment;filename=events.csv');
+      header('Content-Disposition: attachment;filename=export.csv');
 
       //Open up a file pointer
       $fp = fopen('php://output', 'w');

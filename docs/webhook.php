efe1e9309d4badf0ee4b482e1dfc78b58a5250ef
sbenstewart <sbenstewart@gmail.com>
2019-01-31 18:19:12 +0530

    Webhook uploaded

--- docs/webhook.php
+++ docs/webhook.php
@@ -3,24 +3,50 @@ require_once 'dbconfig.php';
 
 
 try {
-    /*$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     //echo "Connected to $dbname at $host successfully.";
 
-      $cid2=$_SESSION["cid"];
-      $count = $conn->exec("insert into registrations(cid, id) values ('$cid2', '$event2')");
-      foreach ($conn->query("SELECT name from user where cid='$cid2'") as $row)
-      {
-        $name = $row['name'];
-      }*/
-      if($json = json_decode(file_get_contents("php://input"), true)) {
-     print_r($json);
+    if($json = json_decode(file_get_contents("php://input"), true)) {
+     //print_r($json);
      $data = $json;
  } else {
      //print_r($_POST);
      $data = $_POST['data'];
-     print_r($data);
      $json = json_decode($data, true);
-     print_r($json);
+     if(is_array($json[0]))
+     {
+       foreach ($json as $temp) {
+         $email=$temp['userEmailId'];
+         $price=$temp['ticketPrice'];
+         $event=$temp['eventName'];
+         echo $email;
+         echo $price;
+         echo $event;
+         foreach ($conn->query("SELECT cid from user where emailid='$email'") as $row)
+         {
+           $cid = $row['cid'];
+         }
+         echo $cid;
+         echo "insert into registrations(cid, id) values ('$cid', '$event')";
+         $count = $conn->exec("insert into registrations(cid, id) values ('$cid', '$event')");
+       }
+     }
+      else {
+     $email=$json['userEmailId'];
+     $price=$json['ticketPrice'];
+     $event=$json['eventName'];
+     echo $email;
+     echo $price;
+     echo $event;
+     foreach ($conn->query("SELECT cid from user where emailid='$email'") as $row)
+     {
+       $cid = $row['cid'];
+     }
+     echo $cid;
+     echo "insert into registrations(cid, id) values ('$cid', '$event')";
+     $count = $conn->exec("insert into registrations(cid, id) values ('$cid', '$event')");
+   }
+
  }
 
 

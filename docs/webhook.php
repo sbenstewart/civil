64dd059134317c9da4234f243606ae4f4097cc3e
sbenstewart <sbenstewart@gmail.com>
2019-01-31 19:10:57 +0530

    Reverted a revert

--- docs/webhook.php
+++ docs/webhook.php
@@ -3,22 +3,15 @@ require_once 'dbconfig.php';
 
 
 try {
-    /*$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     //echo "Connected to $dbname at $host successfully.";
 
-      $cid2=$_SESSION["cid"];
-      $count = $conn->exec("insert into registrations(cid, id) values ('$cid2', '$event2')");
-      foreach ($conn->query("SELECT name from user where cid='$cid2'") as $row)
-      {
-        $name = $row['name'];
-      }*/
-      if($json = json_decode(file_get_contents("php://input"), true)) {
-     print_r($json);
+    if($json = json_decode(file_get_contents("php://input"), true)) {
+     //print_r($json);
      $data = $json;
  } else {
      //print_r($_POST);
      $data = $_POST['data'];
-     //print_r($data);
      $json = json_decode($data, true);
      if(is_array($json[0]))
      {

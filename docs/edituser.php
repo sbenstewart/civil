3710d22ed3bbe081773b907f332110642d9af392
sbenstewart <sbenstewart@gmail.com>
2019-02-03 17:30:40 +0530

    bug fixes

--- docs/edituser.php
+++ docs/edituser.php
@@ -12,9 +12,29 @@ $cid2 = $_POST['cid1'];
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-    $count = $conn->exec("update user set name='$name2', phoneno='$phone2', college='$college2', year='$year2', dept='$dept2', course='$course2' where cid='$cid2' and  emailid='$mail2'");
 
-    echo "Details modified succesfully $id";
+    $sql = "SELECT COUNT(*) from user where cid='$cid2' and  emailid='$mail2'";
+    if ($res = $conn->query($sql)) {
+
+        /* Check the number of rows that match the SELECT statement */
+        if ($res->fetchColumn() > 0) {
+
+          $count = $conn->exec("update user set name='$name2', phoneno='$phone2', college='$college2', year='$year2', dept='$dept2', course='$course2' where cid='$cid2' and  emailid='$mail2'");
+
+          echo "Details modified succesfully $id";
+          }
+
+          else {
+          echo "CID and email id do not match.";
+          }
+
+        }
+
+
+
+
+
+
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }

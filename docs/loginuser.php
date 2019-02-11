c9fcbf78c37ce268f03329fe80cfebb952182492
sbenstewart <sbenstewart@gmail.com>
2019-02-11 20:35:40 +0530

    Login base case

--- docs/loginuser.php
+++ docs/loginuser.php
@@ -4,26 +4,46 @@ $password2 = $_POST['password1'];
 $mail2 = $_POST['mail1'];
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    //echo "Connected to $dbname at $host successfully.";
-    //echo "insert into user(name, emailid, phoneno, college, year, dept, course) values ('$name2', '$mail2', '$phone2', '$college2', '$year2', '$dept2', '$course2')";
+
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+  $sql = "SELECT COUNT(*) from user where emailid='$mail2' and password='$password2'";
+  if ($res = $conn->query($sql)) {
+
+      /* Check the number of rows that match the SELECT statement */
+      if ($res->fetchColumn() > 0) {
+        foreach ($conn->query("SELECT cid,name from user where emailid='$mail2' and password='$password2'") as $row)
+        {
+          $id = $row['cid'];
+          $name = $row['name'];
+          session_start();
+          $_SESSION["cid"]=$id;
+          $_SESSION["name"]=$name;
+          echo "Logged in as ";
+          echo $_SESSION["name"];
+          echo " with cid ";
+          echo $id;
+          echo " Please reload the page to change your details.";
+
+          /*session is started if you don't write this line can't use $_Session  global variable*/
+        }
+
+        }
+        /* No rows matched -- do something else */
+        else {
+        echo "User name / password is incorrect.";
+
+        }
+        }
+        else {
+        echo "Submission failed. Try later.";
+        }
 
 
-    foreach ($conn->query("SELECT cid,name from user where emailid='$mail2' and password='$password2'") as $row)
-    {
-      $id = $row['cid'];
-      $name = $row['name'];
-      /*session is started if you don't write this line can't use $_Session  global variable*/
-    }
-  session_start();
-  $_SESSION["cid"]=$id;
-  $_SESSION["name"]=$name;
 
-  echo "Logged in as ";
-  echo $_SESSION["name"];
-  echo " with cid ";
-  echo $id;
-  echo " Please reload the page to change your details.";
+
+
+
 
 } catch (PDOException $pe) {
     die("Could not connect to the server. Please check your internet connection.");

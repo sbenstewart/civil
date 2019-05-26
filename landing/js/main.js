95ce29e19f59a1685676a5f58fc63de741939f5c
sbenstewart <sbenstewart@gmail.com>
2019-05-26 09:44:04 +0530

    landing page

--- landing/js/main.js
+++ landing/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function($) {
     var Body = $('body');
     Body.addClass('preloader-site');
@@ -9,16 +8,4 @@ $(window).on('load',function() {
     $('body').removeClass('preloader-site');
     var scene = $('#scene').get(0);
     var parallaxInstance = new Parallax(scene);
-=======
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-$(window).on('load',function() {
-    
-    $('.preloader-wrapper').delay(800).fadeOut(800);
-    $('body').removeClass('preloader-site');
-    var scene = $('#scene').get(0);
-    var parallaxInstance = new Parallax(scene);
->>>>>>> sandy
 });

3c6a030232c54e608c070149dcadeb204ab4e6b5
sbenstewart <sbenstewart@gmail.com>
2019-12-21 14:04:15 +0530

    admin password hashed

--- docs/loginadmin.php
+++ docs/loginadmin.php
@@ -9,16 +9,27 @@ try {
     //echo "insert into user(name, emailid, phoneno, college, year, dept, course) values ('$name2', '$mail2', '$phone2', '$college2', '$year2', '$dept2', '$course2')";
     //echo "SELECT cid from admin where emailid='$mail2' and password='$password2'";
 
-    foreach ($conn->query("SELECT cid from admin where emailid='$mail2' and password='$password2'") as $row)
+    foreach ($conn->query("SELECT cid,password from admin where emailid='$mail2'") as $row)
     {
+      //to be used to find new admin password hash
+      //$loaded = password_hash($password2, PASSWORD_BCRYPT);
+      //echo $loaded;
+      $passwordIsCorrect = password_verify($password2, $row['password']);
+      if ($passwordIsCorrect == false)
+      {
+      echo "Incorrect password";
+      break;
+      }
       $id = $row['cid'];
+      session_start();
+      $_SESSION["admin"]=$id;
+
+      echo "Logged in as ";
+      echo $_SESSION["admin"];
+      break;
       /*session is started if you don't write this line can't use $_Session  global variable*/
     }
-  session_start();
-  $_SESSION["admin"]=$id;
-
-  echo "Logged in as ";
-  echo $_SESSION["admin"];
+  
 
 } catch (PDOException $pe) {
     die("Could not connect to the server. Please check your internet connection.");

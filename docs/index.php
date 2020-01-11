81ed7e5f5a20bf791bbd548419ae7c765f165f77
sbenstewart <sbenstewart@gmail.com>
2020-01-11 20:12:01 +0530

    workshop sample

--- docs/index.php
+++ docs/index.php
@@ -821,13 +821,13 @@
                 <!--Only three workshops use this<div class="col-12 col-sm-6 col-md-4">-->
                 <div class="next-event-wrap">
                     <figure>
-                        <a href="sample-workshop.php"><img src="images/next1.jpg" alt="1"></a>
+                        <a href="sample-workshop.php"><img src="images/workshop.png" alt="1"></a>
 
                         <div class="event-rating">5-3-20</div>
                     </figure>
 
                     <header class="entry-header">
-                        <h3 class="entry-title">Sample Workshop</h3>
+                        <h3 class="entry-title">Workshop 1</h3>
 
                         <div class="posted-date">Thursday <span>March 5, 2020</span></div>
                         <div class="posted-date">Vivek Audi <span>8 AM</span></div>
@@ -842,7 +842,7 @@
             <div class="col-6 col-lg-4 col-xl-4">
                 <div class="next-event-wrap">
                     <figure>
-                        <a href="#"><img src="images/next1.jpg" alt="1"></a>
+                        <a href="#"><img src="images/workshop.png" alt="1"></a>
 
                         <div class="event-rating">6-3-20</div>
                     </figure>
@@ -866,7 +866,7 @@
             <div class="col-6 col-lg-4 col-xl-4">
                 <div class="next-event-wrap">
                     <figure>
-                        <a href="#"><img src="images/next1.jpg" alt="1"></a>
+                        <a href="#"><img src="images/workshop.png" alt="1"></a>
 
                         <div class="event-rating">7-3-20</div>
                     </figure>
@@ -887,13 +887,13 @@
             <div class="col-6 col-lg-4 col-xl-4">
                 <div class="next-event-wrap">
                     <figure>
-                        <a href="#"><img src="images/next1.jpg" alt="1"></a>
+                        <a href="#"><img src="images/workshop.png" alt="1"></a>
 
                         <div class="event-rating">7-3-20</div>
                     </figure>
 
                     <header class="entry-header">
-                        <h3 class="entry-title">Workshop 3</h3>
+                        <h3 class="entry-title">Workshop 4</h3>
 
                         <div class="posted-date">Saturday <span>March 7, 2020</span></div>
                         <div class="posted-date">Vivek Audi <span>8 AM</span></div>

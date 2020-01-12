38fd46f4dfe2a23d8d6ba8986ac3f1a3a3dcdf4e
sbenstewart <sbenstewart@gmail.com>
2020-01-12 20:10:40 +0530

    confloat

--- docs/index.php
+++ docs/index.php
@@ -1217,6 +1217,22 @@
                             </div><!-- .entry-footer" -->
                         </div><!-- .swiper-slide -->
 
+                        <div class="swiper-slide">
+                            <figure>
+                                <img src="images/confloat.png" alt="">
+
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="confloat.php">+</a>
+                            </figure><!-- .hero-image -->
+
+                            <div class="entry-header">
+                                <h2 class="entry-title">Confloat</h2>
+                            </div><!--- .entry-header -->
+
+                            <div class="entry-footer">
+                                <div class="posted-date">Saturday <span>Jan 27, 2018</span></div>
+                            </div><!-- .entry-footer" -->
+                        </div><!-- .swiper-slide -->
+
 
                     </div><!-- .swiper-wrapper -->
 

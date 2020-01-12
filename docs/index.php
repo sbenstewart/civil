40d58d42dc26acf7dc448783ac630d20e111aacd
sbenstewart <sbenstewart@gmail.com>
2020-01-12 20:50:41 +0530

    shipwreck

--- docs/index.php
+++ docs/index.php
@@ -990,13 +990,13 @@
 
                         <div class="swiper-slide">
                             <figure>
-                                <img src="images/event.png" alt="">
+                                <img src="images/shipwreck.png" alt="">
 
-                                <a class="event-overlay-link flex justify-content-center align-items-center" href="sample-event.php">+</a>
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="shipwreck.php">+</a>
                             </figure><!-- .hero-image -->
 
                             <div class="entry-header">
-                                <h2 class="entry-title">Music Concert</h2>
+                                <h2 class="entry-title">Shipwreck</h2>
                             </div><!--- .entry-header -->
 
                             <div class="entry-footer">

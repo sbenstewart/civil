3ec748ccadf21d8b7bddc39f7152fbcf77bf3c7d
sbenstewart <sbenstewart@gmail.com>
2020-01-12 12:40:47 +0530

    archibuildo

--- docs/index.php
+++ docs/index.php
@@ -1075,13 +1075,13 @@
 
                         <div class="swiper-slide">
                             <figure>
-                                <img src="images/event.png" alt="">
+                                <img src="images/arhcibuildo.png" alt="">
 
-                                <a class="event-overlay-link flex justify-content-center align-items-center" href="sample-event.php">+</a>
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="Archibuildo.php">+</a>
                             </figure><!-- .hero-image -->
 
                             <div class="entry-header">
-                                <h2 class="entry-title">Broadway Hit </h2>
+                                <h2 class="entry-title">Arhcibuildo </h2>
                             </div><!--- .entry-header -->
 
                             <div class="entry-footer">

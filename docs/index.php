3eccbada1ba77d458db6676d969604688eae4ee6
sbenstewart <sbenstewart@gmail.com>
2020-01-12 13:30:45 +0530

    posterize

--- docs/index.php
+++ docs/index.php
@@ -1155,13 +1155,13 @@
 
                         <div class="swiper-slide">
                             <figure>
-                                <img src="images/event.png" alt="">
+                                <img src="images/posterize.png" alt="">
 
-                                <a class="event-overlay-link flex justify-content-center align-items-center" href="sample-event.php">+</a>
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="Posterize.php">+</a>
                             </figure><!-- .hero-image -->
 
                             <div class="entry-header">
-                                <h2 class="entry-title">U2 Concert </h2>
+                                <h2 class="entry-title">Posterize</h2>
                             </div><!--- .entry-header -->
 
                             <div class="entry-footer">

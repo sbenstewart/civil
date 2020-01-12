68b833c5f9506e59f4a8b3a0d8c48bd0db1966c9
sbenstewart <sbenstewart@gmail.com>
2020-01-12 13:26:47 +0530

    paper presentation

--- docs/index.php
+++ docs/index.php
@@ -1139,13 +1139,13 @@
 
                         <div class="swiper-slide">
                             <figure>
-                                <img src="images/event.png" alt="">
+                                <img src="images/paperpresentation.png" alt="">
 
-                                <a class="event-overlay-link flex justify-content-center align-items-center" href="sample-event.php">+</a>
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="Paperpresentation.php">+</a>
                             </figure><!-- .hero-image -->
 
                             <div class="entry-header">
-                                <h2 class="entry-title">EDM Festival</h2>
+                                <h2 class="entry-title">Paper<br>Presentation</h2>
                             </div><!--- .entry-header -->
 
                             <div class="entry-footer">

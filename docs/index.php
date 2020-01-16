c59560ae41b3655942655908df24d64afdeb5a77
sbenstewart <sbenstewart@gmail.com>
2020-01-16 13:29:23 +0530

    meme indexed

--- docs/index.php
+++ docs/index.php
@@ -1295,13 +1295,13 @@
 
                         <div class="swiper-slide">
                             <figure>
-                                <img src="images/event.png" alt="">
+                                <img src="images/onlinememecontest.png" alt="">
 
-                                <a class="event-overlay-link flex justify-content-center align-items-center" href="sample-event.php">+</a>
+                                <a class="event-overlay-link flex justify-content-center align-items-center" href="OnlineMemeContest.php">+</a>
                             </figure><!-- .hero-image -->
 
                             <div class="entry-header">
-                                <h2 class="entry-title">Broadway Hit </h2>
+                                <h2 class="entry-title">Meme Contest</h2>
                             </div><!--- .entry-header -->
 
                             <div class="entry-footer">

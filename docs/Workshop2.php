3e4799664bbe23a7ecea2b3425b589e122924694
gsanthosh98 <43073833+gsanthosh98@users.noreply.github.com>
2020-01-21 22:38:14 +0530

    workshop 2

---
+++ docs/Workshop2.php
@@ -0,0 +1,332 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Civilisation</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="This is the official website of Civilisation CEG, a nation level intercollege technical symposium conducted by the Society of Civil Engineers in association with the department of Civil Engineering, College of Engineering Guindy.">
+    <meta name="author" content="Ben Stewart S">
+    <meta name="keywords" content="civilisation,ceg,symposium,2020,civil,ben stewart,chennai,guindy,college">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="css/swiper.min.css">
+
+    <!--Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/toastr.min.css">
+    <script src="js/custom.js"></script>
+</head>
+<body class="single-event-page">
+<header class="site-header">
+<div class="header-bar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-10 col-lg-2 order-lg-1">
+                    <div class="site-branding">
+                        <div class="site-title">
+                            <a href="index.php"><img src="images/weblogo.png" alt="CIVIL"></a>
+                        </div><!-- .site-title -->
+                    </div><!-- .site-branding -->
+                </div><!-- .col -->
+
+                <div class="col-2 col-lg-8 order-3 order-lg-2">
+                    <nav class="site-navigation">
+                        <div class="hamburger-menu d-lg-none">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div><!-- .hamburger-menu -->
+
+                        <ul>
+                            <li><a href="index.php#workshops">WORKSHOPS</a></li>
+                            <li><a href="index.php#events">EVENTS</a></li>
+                            <li><a href="index.php#lectures">LECTURES</a></li>
+                            <li><a href="accomodation.php">ACCOMMODATION</a></li>
+                            <li><a href="about.php">CONTACT</a></li>
+                            <?php
+                            // Start the session
+
+                            if(isset($_SESSION["cid"]))
+                            {
+                                $print1 ='<li class="d-lg-none"><a href="php/logout.php">LOGOUT</a></li>';
+                            }
+                            else {
+                                $print1 ='<li class="d-lg-none"><a href="signup.php">LOGIN</a></li>';
+                            }
+                            echo  $print1;
+                            ?>
+                        </ul>
+                    </nav><!-- .site-navigation -->
+                </div><!-- .col -->
+
+                <div class="col-lg-2 d-none d-lg-block order-2 order-lg-3">
+                    <div class="buy-tickets">
+
+                        <?php
+                            // Start the session
+
+                            if(isset($_SESSION["cid"]))
+                            {
+                                $print1 ='<a class="btn gradient-bg" href="php/logout.php">LOGOUT</a>';
+                            }
+                            else {
+                                $print1 ='<a class="btn gradient-bg" href="signup.php">LOGIN</a>';
+                            }
+                            echo  $print1;
+                            ?>
+
+
+                    </div><!-- .buy-tickets -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container-fluid -->
+    </div><!-- .header-bar -->
+
+    <div class="page-header single-event-page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <header class="entry-header">
+                        <h1 class="entry-title">Pre Stressed Concrete Bridges</h1>
+                    </header>
+                </div>
+            </div>
+        </div>
+    </div>
+</header><!-- .site-header -->
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-12">
+            <div class="tabs">
+                <ul class="tabs-nav flex">
+                    <li class="tab-nav flex justify-content-center align-items-center" data-target="#tab_details">Details</li>
+                    <li class="tab-nav flex justify-content-center align-items-center" data-target="#tab_venue">Venue</li>
+                    <li class="tab-nav flex justify-content-center align-items-center" data-target="#tab_organizers">Organisers</li>
+                    <li class="tab-nav flex justify-content-center align-items-center" data-target="#tab_about">Register</li>
+                </ul>
+
+                <div class="tabs-container">
+                    <div id="tab_details" class="tab-content">
+
+
+                        <p>This event is conducted by</p>
+
+                        <h2 class="entry-header">LARSEN & TOUBRO</h2>
+                        <!--<p><strong>Vice President, Head of Public Space Business Unit of L&T Construction.</strong></p>-->
+                        <p>
+                        In the field of bridge engineering, the introduction of prestressed concrete has aided the construction of Long-span concrete bridges. However, deterioration of these bridges before the end of their stipulated design life poses a challenging yet intriguing question to us Civil Engineers. This workshop hopes to inculcate knowledge we already possess and to train young budding minds to come up with innovative solutions to uncommon situations.  It also holds  an overview of such innovative technologies on prestressed concrete bridges ,their development & application in real time construction projects. Some noteworthy structures which represent the state- of -the- art technologies in the construction of prestressed concrete bridges  will also be presented.
+                        </p>
+                       <!--<<p><strong>You will learn about the Statue of Unity.</strong></p>-->
+
+                    </div>
+
+                    <div id="tab_venue" class="tab-content">
+
+                        <div class="flex flex-wrap justify-content-between">
+                            <div class="single-event-details">
+
+
+                                <div class="single-event-details-row">
+                                    <label>Venue:</label>
+                                    <p>Vivek Auditorium</p>
+                                </div>
+
+                                <div class="single-event-details-row">
+                                    <label>Time:</label>
+                                    <p>9 AM – 4 PM</p>
+                                </div>
+
+                                <div class="single-event-details-row">
+                                    <label>Time:</label>
+                                    <p>6<sup>th</sup>March 2020</p>
+                                </div>
+
+
+
+                            </div>
+
+                            <div class="single-event-map">
+                                <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3858741907034!2d80.2341250145436!3d13.011081590829994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52679f425b23db%3A0xe116c31bfc5cb0cb!2sVivekananda%20Auditorium!5e0!3m2!1sen!2sus!4v1578238631469!5m2!1sen!2sus" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="tab_organizers" class="tab-content">
+                        <h2 class="entry-header">Contact:</h2>
+                        <ul>
+                        <li>	Raj Aditya&emsp;9629230056</li>
+                        <li>	Priya&emsp;8675604979</li>
+                        <li>	Dhivya&emsp;8489126689</li>
+                        </ul>
+                        <p>You can also mail us at civilisationworkshop19@gmail.com</p>
+
+                    </div>
+
+                    <div id="tab_about" class="tab-content">
+                        <p><strong>Click the button below to register.</strong></p><br>
+                        <p>The workshop has been scheduled for March 5.</p><br>
+                        <p>The workshop costs INR 800 for one indiviual. Group registrations have to be done onspot. T-Shirts and Food can be availed only for online payments.</p>
+                        <?php
+                        // Start the session
+
+                            if(isset($_SESSION["cid"]))
+                            {
+                            $print2 ="<p><strong>You are currently logged in as ".$_SESSION["name"]."</strong></p>";
+                            }
+                            else {
+                                $print2 ="<p><strong>You must be logged in to register online or onspot.</strong></p>";
+                              }
+                            echo  $print2;
+                        ?>
+
+<form id="registration" class="entry-content">
+
+<!--<div class="entry-content flex">-->
+    <input type="hidden" value="SAMPLE WORKSHOP" id="event" />
+    <input class="btn gradient-bg flex justify-content-center align-items-center" type="button" onclick="regEvent()" value="Pay onspot"></input>
+    <br>
+    <?php
+    // Start the session
+    if(isset($_SESSION["cid"]))
+    {
+
+    $print ='
+    <div class="entry-content flex">
+        <a href="https://www.townscript.com/e/tall-structures-232334" class="btn gradient-bg flex justify-content-center align-items-center">Pay and Register</a>
+    </div>
+    ';
+    }
+    else {
+    $print ='
+    <div class="entry-content flex">
+        <a href="login.php" class="btn gradient-bg flex justify-content-center align-items-center">Pay and Register</a>
+    </div>
+    ';
+    }
+    echo  $print;
+    ?>
+
+
+<!--</div>-->
+</form>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-12">
+            <div class="upcoming-events">
+                <div class="upcoming-events-header">
+                    <h4>Upcoming Events</h4>
+                </div>
+
+                <div class="upcoming-events-list">
+                    <div class="upcoming-event-wrap flex flex-wrap justify-content-between align-items-center">
+                        <figure class="events-thumbnail">
+                            <a href="#"><img src="images/upcoming-1.jpg" alt=""></a>
+                        </figure>
+
+                        <div class="entry-meta">
+                            <div class="event-date">
+                                25<span>February</span>
+                            </div>
+                        </div>
+
+                        <header class="entry-header">
+                            <h3 class="entry-title"><a href="#">Blockchain Conference</a></h3>
+
+                            <div class="event-date-time">May 29, 2018 @ 8:00 Pm - May 30, 2018 @ 4:00 Am</div>
+
+                            <div class="event-speaker">Speackers: Maria Williams, Luis Smith, James Doe</div>
+                        </header>
+
+                        <footer class="entry-footer">
+                            <a href="#">Buy Tikets</a>
+                        </footer>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <figure class="footer-logo">
+                    <a href="#"><p class="entry-title">Civilisation 2020</p></a>
+                </figure>
+
+                <nav class="footer-navigation">
+                    <ul class="flex flex-wrap justify-content-center align-items-center">
+                        <li><a href="#workshops">WORKSHOPS</a></li>
+                            <li><a href="#events">EVENTS</a></li>
+                            <li><a href="#lectures">LECTURES</a></li>
+                            <li><a href="accomodation.php">ACCOMMODATION</a></li>
+                            <li><a href="about.php">CONTACT</a></li>
+                    </ul>
+                </nav>
+
+                Made with <i class="fa fa-heart" aria-hidden="true"></i> in <a href="https://www.google.com/maps/place/College+of+Engineering,+Guindy/@13.0169607,80.2036514,13z/data=!4m5!3m4!1s0x3a52679f0d20f797:0x59f9f10c66e02a19!8m2!3d13.01094!4d80.2354462" target="_blank">Chennai</a>
+                <div class="footer-social">
+                    <ul class="flex flex-wrap justify-content-center align-items-center">
+                                <li><a href="https://fb.com/au.civilisation"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="https://instagram.com/civilisation20"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="tel:+919176224940"><i class="fa fa-phone"></i></a></li>
+                                <li><a href="mailto:sceceg1932@gmail.com?Subject=Help%20Needed" target="_top"><i class="fa fa-envelope"></i></a></li>
+                                <!--<li><a href="#"><i class="fa fa-address-book"></i></a></li>-->
+                                <li><a href="https://api.whatsapp.com/send?phone=919176224940"><i class="fa fa-whatsapp"></i></a></li>
+                                <li><a href="https://forms.gle/DoiPfyzcotmjpasa6"><i class="fa fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="back-to-top flex justify-content-center align-items-center">
+    <span><svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1395 1184q0 13-10 23l-50 50q-10 10-23 10t-23-10l-393-393-393 393q-10 10-23 10t-23-10l-50-50q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l466 466q10 10 10 23z"/></svg></span>
+</div>
+
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/masonry.pkgd.min.js'></script>
+<script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+<script type='text/javascript' src='js/swiper.min.js'></script>
+<script type='text/javascript' src='js/jquery.countdown.min.js'></script>
+<script type='text/javascript' src='js/circle-progress.min.js'></script>
+<script type='text/javascript' src='js/jquery.countTo.min.js'></script>
+<script type='text/javascript' src='js/custom.js'></script>
+<script type='text/javascript' src='js/main.js'></script>
+<script type='text/javascript' src='js/toastr.min.js'></script>
+
+</body>
+</html>

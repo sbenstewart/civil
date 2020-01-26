84827292351937ac4fd0fbd1759737a8f53602f7
sbenstewart <sbenstewart@gmail.com>
2020-01-26 21:14:52 +0530

    forgot password fixed

--- docs/php/forgotpassword.php
+++ docs/php/forgotpassword.php
@@ -34,7 +34,7 @@ try {
         if ($res->fetchColumn() > 0) {
 		  $code2 = password_hash($code, PASSWORD_BCRYPT);
           $count = $conn->exec("update user set password='$code2' where emailid='$mail2'");
-          echo "New password is $code";
+          //echo "New password is $code";
 
 					$to   = $mail2;
 					$from = 'help@civilisationceg.in';
